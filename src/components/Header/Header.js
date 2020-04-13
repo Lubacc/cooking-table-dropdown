02e94f62cc4072467060a6360e3be6b0d036be82
Lubacc <lyubomir.vaklinov@gmail.com>
2020-04-13 13:02:44 +0300

    Test created

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import { AppBar, Toolbar, Typography } from '@material-ui/core';
 import { Box } from '@material-ui/core';
 
 export default (Header) => (
-  <AppBar position="static">
+  <AppBar data-test="appBar" position="static">
     <Toolbar>
       <Box
         display="flex"
